d9a4d202b9bf071487cd0765ca244dbdbec6794c
Administrator <sdfA>
2017-09-20 17:37:37 +0800

    springboot-login

--- src/main/java/com/hls/springboot/security/controller/LoginController.java
+++ src/main/java/com/hls/springboot/security/controller/LoginController.java
@@ -71,7 +71,7 @@ public class LoginController {
     }
 
     /**
-     * 注销
+     * 注销j
      */
     @RequestMapping(value = "/loginout", method = RequestMethod.GET)
     public String loginOut(HttpSession session) {
